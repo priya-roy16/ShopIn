a3898c383faa67003eab77ba018571caae781804
Priya Roy <17bce103@nirmauni.ac.in>
2021-01-05 20:30:27 +0530

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,37 @@
-// import express from 'express';
-var express = require('express')
-// import data from './data';
-//var data = require('./data')
+import express from 'express';
+import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import config from './config';
+import userRoute from './routes/userRoute';
+import productRoute from './routes/productRoute';
+import orderRoute from './routes/orderRoute';
+import uploadRoute from './routes/uploadRoute';
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch((error) => console.log(error.reason));
+
 const app = express();
-
-app.get("/api/products",(req,res)=>{
-    res.send(data.products);
-})
-
-app.listen(5000, () => {
-    console.log('Server started at http://localhost:5000');
+app.use(bodyParser.json());
+app.use('/api/uploads', uploadRoute);
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/orders', orderRoute);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(config.PAYPAL_CLIENT_ID);
+});
+app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
 
+app.listen(config.PORT, () => {
+  console.log('Server started at http://localhost:5000');
+});
